3ea55b1e6cbffea37c41656b13693554eec9b6f1
hossein1387 <m.h.askari.hemmat@gmail.com>
2015-09-13 22:17:05 -0400

    Added python code for BART System

--- BART/src/SimpleGUITk-1.1.3/simpleplot/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2013 David Holm <dholmster@gmail.com>
-# This file is part of SimpleGUITk - https://github.com/dholm/simpleguitk
-# See the file 'COPYING' for copying permission.
-
-from .plot import plot_lines
